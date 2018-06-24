607b1ed88a78695498c5ae98e4c68e0cd584fdd0
Roman Averin <rastler@gmail.com>
2018-06-24 17:04:24 +0300

    fix select conf

--- src-backend/models/index.js
+++ src-backend/models/index.js
@@ -12,7 +12,7 @@ const config = JSON.parse(configFile)[env];
 const db = {};
 let sequelize;
 
-if (config.use_env_constiable) {
+if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_constiable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, { ...config, logging: msg => debug('%s', msg) });
